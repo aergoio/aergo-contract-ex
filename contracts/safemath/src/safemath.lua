bfb137925cf6e6539b0a9ac7fd895f5ae9007d6a
Bernardo Ramos <bernardo@blocko.io>
2022-01-26 15:56:35 -0300

    Update safemath.lua

--- contracts/safemath/src/safemath.lua
+++ contracts/safemath/src/safemath.lua
@@ -11,7 +11,7 @@ function safemath.add(a, b)
 
   local c = a + b
   assert(bignum.compare(c, a) >= 0)
-  
+
   return c
 end
 
@@ -27,24 +27,27 @@ end
 
 function safemath.mul(a, b) 
   a = bignum.number(a or 0)  
-  -- when a == 0
-  if bignum.compare(a, 0) == 0 then 
-    return bignum.number(0)
-  end
-  
   b = bignum.number(b or 0)
-  
+  -- when a == 0
+  if bignum.iszero(a) then 
+    return a
+  end
+  -- when b == 0
+  if bignum.iszero(b) then 
+    return b
+  end
+
   local c = a * b
   assert(c/a == b, "overflow")
-  
+
   return c
 end
 
 function safemath.div(a, b)
   a = bignum.number(a or 0)
   b = bignum.number(b or 0)
-   
-  assert(bignum.compare(b, 0) > 0, "divide by zero")
+
+  assert(bignum.compare(b, 0) > 0, "division by zero")
   c = a / b
 
   return c
@@ -53,8 +56,8 @@ end
 function safemath.mod(a, b)
   a = bignum.number(a or 0)
   b = bignum.number(b or 0)
-  
-  assert(bignum.compare(b, 0) ~= 0, "divide by zero")
+
+  assert(bignum.compare(b, 0) ~= 0, "division by zero")
   c = a % b
 
   return c
